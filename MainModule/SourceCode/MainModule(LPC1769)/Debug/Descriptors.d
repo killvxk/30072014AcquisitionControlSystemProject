9ea34dca86f9e3de5bacf196283069431bea6034
nix- <srezoski.nikica@gmail.com>
2014-09-04 12:55:17 +0200

    Added Ignored files

---
+++ MainModule/SourceCode/MainModule(LPC1769)/Debug/Descriptors.d
@@ -0,0 +1,357 @@
+Descriptors.d: ../Descriptors.c ../Descriptors.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/USB.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/../../Common/Common.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/../../Common/../LPCUSBlibConfig.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/../../Common/CompilerSpecific.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/../../Common/Attributes.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/../../Common/Endianness.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBMode.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/../../../LPCUSBlibConfig.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBTask.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/../../../Common/Common.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBMode.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBController.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Events.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBTask.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBInterrupt.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Device.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/StdDescriptors.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Endpoint.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/Endpoint_LPC17xx.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../EndpointCommon.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../HAL/HAL.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../HAL/LPC17XX/HAL_LPC17xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/chip.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_board\boards_17xx_40xx\nxp_xpresso_1769/sys_config.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/chip_lpc175x_6x.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/lpc_types.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/adc_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/cmsis.h \
+ C:\LPCOpen\lpcopen\software\CMSIS\CMSIS\Include/core_cm3.h \
+ C:\LPCOpen\lpcopen\software\CMSIS\CMSIS\Include/core_cmInstr.h \
+ C:\LPCOpen\lpcopen\software\CMSIS\CMSIS\Include/core_cmFunc.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/can_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/dac_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/enet_002.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/gpdma_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/gpio_002.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/i2c_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/i2s_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/mcpwm_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/qei_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/regfile_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/rtc_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/ssp_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/spi_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/timer_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/usart_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/wwdt_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/usb_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/ritimer_001.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/gpio_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/uart_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/gpdma_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/i2c_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/i2s_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/spi_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/ssp_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/rtc_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/adc_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/dac_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/timer_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/iocon_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/sysctl_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/clock_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/fmc_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/can_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/enet_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/wwdt_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/ritimer_17xx_40xx.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../HAL/LPC17XX/../../USBMode.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/Device_LPC17xx.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../../../../Common/Common.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../USBController.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../StdDescriptors.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../USBInterrupt.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../HAL/HAL.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../Endpoint.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DeviceStandardReq.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/StdRequestType.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/EndpointStream.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Events.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/StdDescriptors.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/ConfigDescriptor.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/HostStandardReq.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBController.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBInterrupt.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Device.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Endpoint.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DeviceStandardReq.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/EndpointStream.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/AudioClass.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/../Core/USBMode.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/AudioClassDevice.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../../USB.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/AudioClassCommon.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/../../Core/StdDescriptors.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/CDCClass.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/CDCClassDevice.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/CDCClassCommon.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/HIDClass.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/HIDClassDevice.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/HIDClassCommon.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/HIDParser.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/../../../../Common/Common.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/HIDReportData.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/HIDClassCommon.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/MassStorageClass.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/MassStorageClassDevice.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/MassStorageClassCommon.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/MIDIClass.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/MIDIClassDevice.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/MIDIClassCommon.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/AudioClassCommon.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/PrinterClass.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/RNDISClass.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/RNDISClassDevice.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/RNDISClassCommon.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/CDCClassCommon.h \
+ C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/StillImageClass.h
+
+../Descriptors.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/USB.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/../../Common/Common.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/../../Common/../LPCUSBlibConfig.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/../../Common/CompilerSpecific.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/../../Common/Attributes.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/../../Common/Endianness.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBMode.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/../../../LPCUSBlibConfig.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBTask.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/../../../Common/Common.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBMode.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBController.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Events.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBTask.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBInterrupt.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Device.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/StdDescriptors.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Endpoint.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/Endpoint_LPC17xx.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../EndpointCommon.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../HAL/HAL.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../HAL/LPC17XX/HAL_LPC17xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/chip.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_board\boards_17xx_40xx\nxp_xpresso_1769/sys_config.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/chip_lpc175x_6x.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/lpc_types.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/adc_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/cmsis.h:
+
+C:\LPCOpen\lpcopen\software\CMSIS\CMSIS\Include/core_cm3.h:
+
+C:\LPCOpen\lpcopen\software\CMSIS\CMSIS\Include/core_cmInstr.h:
+
+C:\LPCOpen\lpcopen\software\CMSIS\CMSIS\Include/core_cmFunc.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/can_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/dac_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/enet_002.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/gpdma_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/gpio_002.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/i2c_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/i2s_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/mcpwm_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/qei_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/regfile_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/rtc_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/ssp_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/spi_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/timer_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/usart_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/wwdt_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/usb_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_ip/ritimer_001.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/gpio_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/uart_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/gpdma_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/i2c_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/i2s_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/spi_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/ssp_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/rtc_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/adc_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/dac_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/timer_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/iocon_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/sysctl_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/clock_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/fmc_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/can_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/enet_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/wwdt_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_chip\chip_17xx_40xx/ritimer_17xx_40xx.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../HAL/LPC17XX/../../USBMode.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/Device_LPC17xx.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../../../../Common/Common.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../USBController.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../StdDescriptors.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../USBInterrupt.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../HAL/HAL.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DCD/LPC17XX/../../Endpoint.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DeviceStandardReq.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/StdRequestType.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/EndpointStream.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Events.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/StdDescriptors.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/ConfigDescriptor.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/HostStandardReq.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBController.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/USBInterrupt.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Device.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/Endpoint.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/DeviceStandardReq.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Core/EndpointStream.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/AudioClass.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/../Core/USBMode.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/AudioClassDevice.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../../USB.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/AudioClassCommon.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/../../Core/StdDescriptors.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/CDCClass.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/CDCClassDevice.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/CDCClassCommon.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/HIDClass.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/HIDClassDevice.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/HIDClassCommon.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/HIDParser.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/../../../../Common/Common.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/HIDReportData.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/HIDClassCommon.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/MassStorageClass.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/MassStorageClassDevice.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/MassStorageClassCommon.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/MIDIClass.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/MIDIClassDevice.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/MIDIClassCommon.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/AudioClassCommon.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/PrinterClass.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/RNDISClass.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/RNDISClassDevice.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/RNDISClassCommon.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/Device/../Common/CDCClassCommon.h:
+
+C:\LPCOpen\lpcopen\software\LPCUSBLib\Drivers\USB/Class/StillImageClass.h:
---
+++ MainModule/SourceCode/MainModule(LPC1769)/Debug/cr_startup_lpc17xx40xx.d
@@ -0,0 +1,4 @@
+cr_startup_lpc17xx40xx.d: ../cr_startup_lpc17xx40xx.c \
+ C:\LPCOpen\lpcopen\software\lpc_core\lpc_board\boards_17xx_40xx\nxp_xpresso_1769/sys_config.h
+
+C:\LPCOpen\lpcopen\software\lpc_core\lpc_board\boards_17xx_40xx\nxp_xpresso_1769/sys_config.h:
